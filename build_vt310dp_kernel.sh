8b9186d155d1e5f76a382766f55113e2fc62ac97
Andrius V <vezhlys@netscape.net>
2017-08-15 00:32:54 +0300

    fixing typo in path

--- build_vt310dp_kernel.sh
+++ build_vt310dp_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export KERNEL=VT-310DP
-cd ~/netbsd-src/sys/arch/386/conf
+cd ~/netbsd-src/sys/arch/i386/conf
 config $KERNEL && cd ../compile/$KERNEL && make clean && make depend && make && cp netbsd ~/netbsd-kernels/i386
 
