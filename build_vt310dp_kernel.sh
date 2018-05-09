1a456dd1edb91ef3a903de65098058e57eaf8b76
Andrius V <vezhlys@netscape.net>
2018-05-09 22:05:01 +0300

    add logtime and use 2 cores for building and export KERNEL_HOME property

--- build_vt310dp_kernel.sh
+++ build_vt310dp_kernel.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export KERNEL=VT-310DP
-cd ~/netbsd-src/sys/arch/386/conf
-config $KERNEL && cd ../compile/$KERNEL && make clean && make depend && make && cp netbsd ~/netbsd-kernels/i386
-
+export KERNEL_HOME=$HOME/netbsd-kernels/i386
+cd $HOME/netbsd-src/sys/arch/i386/conf
+logtime > $KERNEL_HOME/starttime.txt
+config $KERNEL && cd ../compile/$KERNEL && make clean && make depend && make -j2 && cp netbsd $KERNEL_HOME
+logtime > $KERNEL_HOME/stoptime.txt
