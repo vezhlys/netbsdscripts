6e9a9aed6ab0fbd55c2c323b260b70d483e95835
Andrius V <vezhlys@netscape.net>
2018-05-29 09:55:12 +0300

    ignore changes on fossil close

--- pkgsrc_update_to_latest_fossil_repo.sh
+++ pkgsrc_update_to_latest_fossil_repo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /usr/pkgsrc &&
-sudo fossil open /usr/pkgsrc/pkgsrc.fossil &&
+sudo fossil open pkgsrc.fossil &&
 sudo fossil pull &&
 sudo fossil update --latest &&
-sudo fossil close
+sudo fossil close -f
 
