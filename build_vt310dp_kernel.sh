bf34e917bd0fde05478023e243e82c743505f8b1
Andrius V <vezhlys@netscape.net>
2019-05-07 02:23:32 +0300

    simplified config for VT-310DP + unichromefb, based on GENERIC v1.1206 (no padlock, viadrmums, use kernel modules), reverting build_vt310dp_kernel.sh to build this config

--- build_vt310dp_kernel.sh
+++ build_vt310dp_kernel.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-#export KERNEL=VT-310DP
-export KERNEL=GENERIC
+#
+# Builds VT-310DP kernel configuration (should be copied from the same repo to sys/arch/i386/conf
+# 
+
+export KERNEL=VT-310DP
+#export KERNEL=GENERIC
 export KERNEL_HOME=$HOME/netbsd-kernels/i386
 cd $HOME/netbsd-src/sys/arch/i386/conf
 logtime > $KERNEL_HOME/starttime.txt
