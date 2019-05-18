be29ba3a20a4a7205ef9b108721601c3bfcb61db
vezhlys <vezhlys@gmail.com>
2019-05-18 19:53:37 +0000

    clone fossil src repo and checkout latest locally

---
+++ netbsd_clone_fossil_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Clones fossil NetBSD repo https://src.fossil.netbsd.org/. 
+# It is assumed that fossil scm is installed already.
+
+export NETBSD_SRC_HOME=$HOME/netbsd-src
+mkdir -p $NETBSD_SRC_HOME
+fossil clone https://src.fossil.netbsd.org $NETBSD_SRC_HOME/netbsd-src.fossil &&
+cd $NETBSD_SRC_HOME &&
+fossil open netbsd-src.fossil &&
+fossil checkout --latest &&
+fossil close
+
