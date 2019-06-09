a603fb8edfe0f3dba1f9f2ab31e2e34cb4c070bc
vezhlys <vezhlys@gmail.com>
2019-06-09 07:45:07 +0000

    setting SQLITE_TMPDIR to home folder to avoid crash on small /tmp partition. script comment fixed/updated too.

--- pkgsrc_clone_fossil_repo.sh
+++ pkgsrc_clone_fossil_repo.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 # Clones fossil pkgsrc repo https://pkgsrc.fossil.netbsd.org/. 
-# It is assumed that fossil scm is installed already and /usr/pkgscr folder is created.
+# It is assumed that fossil scm is installed already and /usr/pkgsrc folder already exists. Script should be executed using sudo.
 
+export SQLITE_TMPDIR=$HOME
 fossil clone https://pkgsrc.fossil.netbsd.org /usr/pkgsrc/pkgsrc.fossil &&
 cd /usr/pkgsrc &&
 fossil open /usr/pkgsrc/pkgsrc.fossil &&
