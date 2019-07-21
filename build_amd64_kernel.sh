29ee5609d4c6b55207437b62093053ab9826fa17
vezhlys <vezhlys@gmail.com>
2019-07-21 20:59:34 +0300

    detect number of active cores from sysctl and utilize all of them for building the kernel.

--- build_amd64_kernel.sh
+++ build_amd64_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export KERNEL=GENERIC
 cd ~/netbsd-src/sys/arch/amd64/conf
-config $KERNEL && cd ../compile/$KERNEL && make clean && make depend && make -j2 && cp netbsd ~/netbsd-kernels/amd64
+NR_OF_CPUS=$(sysctl -a |grep hw\.ncpuonline|sed 's/[^0-9]*//g')
+config $KERNEL && cd ../compile/$KERNEL && make clean && make depend && make -j$NR_OF_CPUS && cp netbsd ~/netbsd-kernels/amd64
 
