703e20f0f1e50390e73fceab4d29f04b2586fd5c
vezhlys <vezhlys@netscape.net>
2020-11-20 01:07:55 +0200

    comment cleanup for pkgsrc, since it fails with the latest fossil

--- pkgsrc_update_to_latest_fossil_repo.sh
+++ pkgsrc_update_to_latest_fossil_repo.sh
@@ -4,6 +4,6 @@ export SQLITE_TMPDIR=$HOME
 cd /usr/pkgsrc &&
 fossil open --force pkgsrc.fossil &&
 fossil update &&
-fossil clean --keep distfiles/*,packages/*  -f &&
+#fossil clean --keep distfiles/*,packages/*  -f &&
 fossil close
 
