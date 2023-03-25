e81670299b1568c7a2675587b43e3a4e6852b61a
Andrius V <vezhlys@netscape.net>
2023-03-25 20:57:53 +0200

    exit, if ~/netbsd-src folder doesn't exist.

--- build_userland.sh
+++ build_userland.sh
@@ -2,7 +2,7 @@
 
 exe="./save_nr_of_cpus.sh"
 NR_OF_CPUS=`$exe`
-cd ~/netbsd-src/
+cd ~/netbsd-src/ || exit
 logtime > ~/netbsdscripts/userland_start.txt
 ./build.sh -r -j$NR_OF_CPUS -O ../obj -T ../tools -U distribution
 logtime > ~/netbsdscripts/userland_stop.txt
